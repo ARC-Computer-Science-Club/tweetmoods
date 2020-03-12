35f7dd6a1688a270fe34ff684ac88792fe306def
Will Sieving <willsieving22@gmail.com>
2020-03-12 00:18:50 -0700

    Added graph functionality
    The graph button now queries the database for all entries by the given handle and returns them to the user.

--- run_server.js
+++ run_server.js
@@ -6,7 +6,7 @@ var today = new Date();
 
 
 
-// opens the connection to the SQLite database (filename = tweets.db)
+// declares the object that will open the connection to the SQLite database (filename = tweets.db)
 function open_con() {
     db = new sqlite3.Database('tweets.db', (err) => {
         if (err) {
@@ -67,7 +67,7 @@ app.post('/handle', urlencodedParser, function (req, res) {
     console.log(handle_entered +'' + ' ' + month + '-' + day + '-' + year + ' ' + hour + ':' + minutes);
     // debug print statement
 
-    let query = 'INSERT INTO handles(handle, mood, month, day, year, hour, minute) VALUES(' + handle_entered + ',1,' + month + ',' + day + ',' + year + ',' + hour + ',' + minutes + ')';
+    let query = `INSERT INTO handles(handle, mood, month, day, year, hour, minute) VALUES('${handle_entered}',1,'${month}','${day}','${year}','${hour}','${minutes}')`;
     db.run(query);
     // insert data into table
 
@@ -92,11 +92,11 @@ app.get('/list', function (req, res) {
         // here we user a FOR loop like in python to print each attribute of every row in the database
         rows.forEach((row) => {
             if (row.minute < 10) {
-                let min = row.minute
-                let zero = 0
+                let min = row.minute;
+                let zero = 0;
                 var minutes = zero.toString() + min.toString();
                 console.log(row.entry_id, row.handle, row.mood, row.month + '-' + row.day + '-' + row.year, row.hour + ':' + minutes);
-                // NOTE: the minutes column doesn't cannot store leading zeros
+                // NOTE: the minutes column cannot store leading zeros
                 // so to combat this problem we add a zero onto it when it is read
                 // Don't forget!
             };
@@ -111,8 +111,40 @@ app.get('/list', function (req, res) {
 });
 
 
-app.get('/graph', function (req, res) {
-    res.send('');
+app.post('/graph', urlencodedParser, function (req, res) {
+
+    if (req.body['handle']) {
+        open_con();
+        // declares database object
+        // doesn't print message because object functions haven't been called yet
+
+        let hand_sel = `SELECT * FROM handles WHERE handle="${req.body['handle']}"`;
+        // set selection criteria
+
+        // db.all is a async function and will be executed after everthing in the code
+        // TO DO: figure out what it's promise is and how that all works
+        // for now I've stuck the res.send and close_con() inside so that they still work
+        db.all(hand_sel, [], (err, rows) => {
+            // db.all() connects to the database and brings all of the contents into memory (expensive if many entries)
+            // then we search through all the entries in memory
+            // this is why we can close the connection in this function, because we are already done with the database (not sure on this)
+
+            // IMPORTANT NOTE:
+            // the open_con(); doesn't open the connection, calling the db object functions does
+            // using db.all, db.each or db.get will connect
+
+            if (err) {
+                throw err
+            };
+            var data_list = rows;
+            close_con();
+            res.send(data_list);
+
+        });
+    } else {
+        res.send('You did not enter a handle. Cannot Query Database.');
+        console.log('passt ds');
+    };
 });
 
 
